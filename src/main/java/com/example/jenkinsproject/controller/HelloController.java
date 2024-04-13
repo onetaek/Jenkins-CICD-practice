8b0258e2597014983ec76b6fbe8fb3a28838f00d
won taek oh <dnjsxorchlrh@naver.com>
2024-04-13 12:05:33 +0900

    환경변수 설정

--- src/main/java/com/example/jenkinsproject/controller/HelloController.java
+++ src/main/java/com/example/jenkinsproject/controller/HelloController.java
@@ -1,11 +1,18 @@
 package com.example.jenkinsproject.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${application.value1}")
+    private String value1;
+
+    @Value("${application.value2}")
+    private String value2;
 
     @GetMapping("/ping")
     public String ping(
@@ -14,4 +21,8 @@ public class HelloController {
         return "good!!\n" + "/param = " + param;
     }
 
+    @GetMapping("/value")
+    public String value() {
+        return String.format("value1: %s, value2: %s",value1, value2);
+    }
 }
