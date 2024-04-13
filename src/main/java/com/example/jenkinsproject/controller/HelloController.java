294740a1341cc420aefcddfa384ff98fdccb2eb2
won taek oh <dnjsxorchlrh@naver.com>
2024-04-13 21:46:03 +0900

    메인 요청 추가

--- src/main/java/com/example/jenkinsproject/controller/HelloController.java
+++ src/main/java/com/example/jenkinsproject/controller/HelloController.java
@@ -1,9 +1,14 @@
 package com.example.jenkinsproject.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 // github push event test 5
 @RestController
@@ -14,6 +19,30 @@ public class HelloController {
 
     @Value("${application.value2}")
     private String value2;
+
+    @GetMapping
+    public Map<String, String> getClientInfo(HttpServletRequest request) {
+        Map<String, String> clientInfo = new HashMap<>();
+        // IP 주소
+        clientInfo.put("ip: ", request.getRemoteAddr());
+        // 포트 번호
+        clientInfo.put("port: ", String.valueOf(request.getRemotePort()));
+        // 요청 메서드
+        clientInfo.put("method: ", request.getMethod());
+        // 요청 URL
+        clientInfo.put("url: ", request.getRequestURL().toString());
+        // 요청 URI
+        clientInfo.put("uri: ", request.getRequestURI());
+        // 쿼리 문자열
+        clientInfo.put("queryString: ", request.getQueryString());
+        // 헤더 정보
+        clientInfo.put("userAgent: ", request.getHeader("User-Agent"));
+
+        clientInfo.put("value1: ", value1);
+
+        clientInfo.put("value2: ", value2);
+        return clientInfo;
+    }
 
     @GetMapping("/ping")
     public String ping(
