cdbf83a296f92aa6f10398f2d17bed84752e6924
won taek oh <dnjsxorchlrh@naver.com>
2024-04-13 20:49:32 +0900

    webhook test3

--- src/main/java/com/example/jenkinsproject/controller/HelloController.java
+++ src/main/java/com/example/jenkinsproject/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-// github push event test 2
+// github push event test 3
 @RestController
 public class HelloController {
 
